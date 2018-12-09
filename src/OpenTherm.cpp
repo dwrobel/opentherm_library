466648a9db1855623ca70fe775a93909b064013d
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2018-12-09 12:31:36 +0100

    Set timeout to 800ms as per spec.

--- src/OpenTherm.cpp
+++ src/OpenTherm.cpp
@@ -180,7 +180,7 @@ void OpenTherm::process()
 
 	if (st == OpenThermStatus::READY) return;
 	unsigned long newTs = micros();
-	if (st != OpenThermStatus::NOT_INITIALIZED && (newTs - ts) > 1000000) {
+	if (st != OpenThermStatus::NOT_INITIALIZED && (newTs - ts) > 800000) {
         status = OpenThermStatus::READY;
 		responseStatus = OpenThermResponseStatus::TIMEOUT;
 		if (processResponseCallback != NULL) {
